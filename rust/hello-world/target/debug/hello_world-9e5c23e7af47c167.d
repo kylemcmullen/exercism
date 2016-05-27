2a5935a2b333f4d73a92036210319089a537c7fe
Kyle McMullen <me@kylemcmullen.net>
2016-05-27 17:35:30 -0400

    initial commit

---
+++ rust/hello-world/target/debug/hello_world-9e5c23e7af47c167.d
@@ -0,0 +1,5 @@
+/Users/kyle/exercism/rust/hello-world/target/debug/hello_world-9e5c23e7af47c167.d: tests/hello-world.rs
+
+/Users/kyle/exercism/rust/hello-world/target/debug/hello_world-9e5c23e7af47c167: tests/hello-world.rs
+
+tests/hello-world.rs:
