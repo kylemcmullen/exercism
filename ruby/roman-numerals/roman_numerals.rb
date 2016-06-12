3ff0961dbd229d88dca06bd0ffc1396594628c65
Kyle McMullen <me@kylemcmullen.net>
2016-06-12 19:15:40 -0400

    hmmm

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -3,35 +3,27 @@ module BookKeeping
 end
 
 class RomanNumerals
-
+  PLACES = [
+    [ 1000 ,      'M'    ],
+    [ 100  , ['C','D','M']],
+    [ 10   , ['X','L','C']],
+    [ 1    , ['I','V','X']]
+  ]
   def self.convert(n)
-    converted = ''
-    converted += convert_thousands(n)
-    converted += convert_hundreds(n)
-    converted += convert_tens(n)
-    converted += convert_ones(n) 
+    PLACES.inject('') do |memo, place_info|
+      memo += convert_place(n,place_info[0],place_info[1])
+    end
   end  
 
   private
-  def self.convert_thousands(n)
-    thousands = (n - (n % 1000)) / 1000
 
-    ('M' * thousands)
-  end
-
-  def self.convert_hundreds(n)
-    hundreds = ((n % 1000) - (n % 100)) / 100
-    RomanNumerals.convert_syms(hundreds, 'C','D','M')
-  end
-
-  def self.convert_tens(n)
-    tens = ((n % 100) - (n % 10)) / 10
-    RomanNumerals.convert_syms(tens, 'X','L','C')
-  end
-
-  def self.convert_ones(n)
-    ones = n % 10
-    RomanNumerals.convert_syms(ones, 'I', 'V', 'X')
+  def self.convert_place(n, place, syms)
+    if syms.is_a? Array
+      count = ((n % (place*10)) - (n % place))/ place
+      convert_syms(count, syms[0],syms[1],syms[2])
+    else
+      syms * ((n - (n % place)) / place)
+    end
   end
 
   def self.convert_syms(n, small, medium, large)
