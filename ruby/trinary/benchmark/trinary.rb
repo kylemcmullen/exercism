35021c3a86bdf18a1d10f495a3cc3728278d250b
Kyle McMullen <me@kylemcmullen.net>
2016-06-27 23:40:48 -0400

    hmmmm

--- ruby/trinary/benchmark/trinary.rb
+++ ruby/trinary/benchmark/trinary.rb
@@ -8,17 +8,15 @@ class Trinary
       digit = string[index]
       case digit
       when '0'
-        # no-op
-        power *= 3
+        # no-op        
       when '1'
-        @value += power
-        power *= 3  
+        @value += power  
       when '2'
-        @value += power + power
-        power *= 3  
+        @value += power + power  
       else
         raise ArgumentError.new("saw a \"#{digit}\"")
       end
+      power *= 3
     end
   rescue ArgumentError => e
     @value = 0
