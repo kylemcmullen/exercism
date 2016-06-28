887be657309f89d74a26ead55d46d93459f8ec61
Kyle McMullen <me@kylemcmullen.net>
2016-06-27 23:30:49 -0400

    make test less lame

--- ruby/trinary/benchmark/benchmark.rb
+++ ruby/trinary/benchmark/benchmark.rb
@@ -6,17 +6,13 @@ require 'randexp'
 trinary_string =/(0|1|2){100000}/.gen 
 old_result = nil
 new_result = nil
+reference = nil
 Benchmark.bmbm do |x|
   x.report("old: ") { old_result = TrinaryOld.new(trinary_string) }
   x.report("new: ") { new_result = Trinary.new(trinary_string) }
+  x.report("reference: ") { reference = trinary_string.to_i(3) }
 end
 
-puts old_result.to_decimal
-puts '-----'
-puts new_result.to_decimal
-puts '-----'
-puts old_result.to_decimal - new_result.to_decimal
-puts '-----'
-reference = trinary_string.to_i(3)
-puts reference - old_result.to_decimal
-puts reference - new_result.to_decimal
+puts "old vs. reference value: #{reference - old_result.to_decimal}"
+puts "new vs. reference value: #{reference - new_result.to_decimal}"
+puts "old vs. new value: #{old_result.to_decimal - new_result.to_decimal}"
